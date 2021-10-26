b6fc5ad20374a6e06b6fd6f991e99f948c1dcc08
15adhami <15adhami@gmail.com>
2021-10-26 05:36:58 -0700

    Add project files.

---
+++ Source/Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using XmlExtensions;
+using UnityEngine;
+using Verse;
+
+namespace TallerTabMenus
+{
+    class Patches
+    {
+		[HarmonyPatch(typeof(ITab_Storage), "OnOpen")]
+		internal static class Patch_ITab_Storage
+		{
+			private static void Prefix(ref Vector2 ___size, ref Vector2 ___WinSize)
+			{
+				___WinSize.y = float.Parse(SettingsManager.GetSetting("imranfishnikos.tallertabmenus", "storage_height"));
+				___size.y = float.Parse(SettingsManager.GetSetting("imranfishnikos.tallertabmenus", "storage_height"));
+			}
+		}
+	}
+}
---
+++ Source/TallerMenus.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace TallerTabMenus
+{
+    public class TallerMenus : Mod
+    {
+		public TallerMenus(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("imranfishnikos.tallertabmenus");
+			harmony.PatchAll();
+		}
+	}
+}
